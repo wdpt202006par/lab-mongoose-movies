5a9d6fe4c28461ae4f65b0e7216ca59b62cc45fb
Nina Quaresma <quaresma.nina@gmail.com>
2020-09-05 16:58:45 +0200

    Iteration 3 + 4

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -4,15 +4,43 @@ const Celebrity = require('../models/Celebrity.js');
 
 
 router.get('/', (req, res, next) => {
-Celebrity.find({
+    Celebrity.find({
 
-}).then(celebritiesFromDb => {
-    res.render('celebrities/index', {
-        celebritiesFromDb: celebritiesFromDb
-    })
-}).catch(err => next(err))   
+    }).then(celebritiesFromDb => {
+        res.render('celebrities/index', {
+            celebritiesFromDb: celebritiesFromDb
+        })
+    }).catch(err => next(err))
 })
 
+router.get('/new', (req, res, next) => {
+    res.render('celebrities/new')
+})
+
+router.post('/', (req, res, next) => {
+    Celebrity.create({
+        name: req.body.name,
+        occupation: req.body.occupation,
+        catchPhrase: req.body.catchPhrase
+    }).then(newCelebrity => {
+        res.redirect('/celebrities')
+    }).catch(err => {
+        next(err)
+    })
+})
+
+router.get('/:id', (req, res, next) => {
+    Celebrity.findById(req.params.id).then((oneCelebrity) => {
+        console.log(oneCelebrity)
+        res.render('celebrities/show', {
+            oneCelebrity: oneCelebrity
+        })
+    }).catch(err => {
+        next(err)
+    })
+})
+
+
+
+
 module.exports = router;
-
-
