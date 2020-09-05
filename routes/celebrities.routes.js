134927b7cb9745dd930dbc1199f0e664d4b20e04
Clementine <clementinenocton@hotmail.fr>
2020-09-05 16:16:22 +0200

    Iteration 3 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,6 +3,19 @@ const router = express.Router();
 
 const Celebrity = require('../models/Celebrity.model');
 
+router.get('/celebrities/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  Celebrity.findById(id)
+    .then((theCelebrity) => res.render('celebrities/show', { celebrity: theCelebrity }))
+    .catch(error => {
+      console.log('Error while retrieving celebrity details: ', error);
+
+      // Call the error-middleware to display the error page to the user
+      next(error);
+    });
+});
+
 router.get('/celebrities', (req, res, next) => {
   Celebrity.find()
   .then((allTheCelebritiesFromDB) => {
