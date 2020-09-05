59b92606300768e5d60b7a9695d0bf8ac25b7c7b
Pierre-Alderic Boegler <pa.boegler@gmail.com>
2020-09-05 16:30:07 +0200

    iteration 2 done

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Celebrity = require('../models/celebrity.js')
-const DB_NAME = 'mongoose-movies'
+const DB_NAME = 'lab-mongoose-movies'
 
 mongoose.connect(`mongodb://localhost/${DB_NAME}`, {
   useCreateIndex: true,
@@ -10,29 +10,28 @@ mongoose.connect(`mongodb://localhost/${DB_NAME}`, {
   console.log('youhou!')
 });
 
-const celebrities = [
-    {
+const celebrities = [{
     name: 'Bruce Willis',
     occupation: 'Actor',
     catchPhrase: 'You can\'t undo the past... but you can certainly not repeat it'
-    },
-    {
+  },
+  {
     name: 'Bruce Springsteen',
     occupation: 'Singer',
     catchPhrase: 'Someday we\'ll look back on this and it will all seem funny.'
-    },
-    {
+  },
+  {
     name: 'Bruce Wayne',
     occupation: 'Superhero Millionaire',
     catchPhrase: 'It\'s not who I am underneath, but what I do that defines me.'
-    }
+  }
 ]
 
 Celebrity.create(celebrities)
-    .then((celebritiesAdded) => {
-        console.log(`${celebrities.length} celebrities added`)
-        mongoose.connection.close()
-    })
-    .catch(err => {
-        next (err)
-    })
+  .then((celebritiesAdded) => {
+    console.log(`${celebrities.length} celebrities added`)
+    mongoose.connection.close()
+  })
+  .catch(err => {
+    next(err)
+  })
