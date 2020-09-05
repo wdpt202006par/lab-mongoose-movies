7d7d10a3d26c2135b02240a66c6c4eaef1e72644
Pierre-Alderic Boegler <pa.boegler@gmail.com>
2020-09-05 17:00:02 +0200

    iteration 4 done

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -13,14 +13,31 @@ router.get('/', (req, res, next) => {
         .catch((err) => next(err));
 })
 
+router.get('/new', (req, res, next) => {
+    res.render('celebrities/new', {})
+})
+
+router.post('/new', (req, res, next) => {
+    Celebrity.create({
+            name: req.body.name,
+            occupation: req.body.occupation,
+            catchPhrase: req.body.catchPhrase
+        }).then(() =>
+            res.redirect('/celebrities')
+        )
+        .catch((err) => {
+            res.redirect('celebrities/new')
+        })
+})
+
 router.get('/:id', (req, res, next) => {
     Celebrity.findById(req.params.id)
-    .then((selectedCelebrity) => {
-        res.render('celebrities/show', {
-            selectedCelebrity: selectedCelebrity
+        .then((selectedCelebrity) => {
+            res.render('celebrities/show', {
+                selectedCelebrity: selectedCelebrity
+            })
         })
-    })
-    .catch((err) => next(err));
+        .catch((err) => next(err));
 })
 
 module.exports = router;
