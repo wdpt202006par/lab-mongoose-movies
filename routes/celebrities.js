6440443e85cec98488e0fdea58bf5acfbdbdb92b
Sarah Damag <sarahdamag@gmail.com>
2020-09-06 12:39:09 +0200

    iteration 1 to 6 done

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -52,3 +52,45 @@ router.post('/celebrities/new', (req, res, next) => {
          res.redirect('/celebrities/new');
       });
 });
+
+//iteration 5
+router.post('/celebrities/:id/delete', (req, res, next) => {
+   const id = req.params.id;
+   Celebrity.findByIdAndRemove(id)
+      .then(() => {
+         res.redirect('/celebrities');
+      })
+      .catch((err) => {
+         next(err);
+      });
+});
+
+//iteration 6
+//afficher le formulaire
+router.get('/celebrities/:id/edit', (req, res, next) => {
+   const id = req.params.id;
+   Celebrity.findOne({ _id: id })
+      .then((celebrityDetails) => {
+         console.log(celebrityDetails);
+         res.render('celebrities/edit', { celebrity: celebrityDetails });
+      })
+      .catch((err) => {
+         next();
+      });
+});
+
+//traitement du formulaire
+router.post('/celebrities/:id/edit', (req, res, next) => {
+   const updatedCelebrity = {
+      name: req.body.name,
+      occupation: req.body.occupation,
+      catchPhrase: req.body.catchPhrase,
+   };
+   Celebrity.findByIdAndUpdate(req.params.id, updatedCelebrity, { new: true })
+      .then(() => {
+         res.redirect('/celebrities');
+      })
+      .catch((err) => {
+         next(err);
+      });
+});
