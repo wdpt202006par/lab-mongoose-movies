d3547175f64b932f9125a38ced691c1811da8483
Enine Chareunsouk <enine.chareunsouk@gmail.com>
2020-09-08 19:17:58 +0200

    #4 done

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -16,23 +16,44 @@ router.get('/celebrities', (req, res, next) => {
       })
     })
 
-router.get('/celebrities/:id', (req, res, next) => {
-    const id = req.params.id // le nom doit être le même que celui de la route
 
-    Celebrity.findOne({_id: id})
-    .then((celebrity) => {
-      console.log('celebrity', celebrity)  
-      res.render('celebrities/show', {
-        celebrity: celebrity
-      })
-    })
-    .catch(err => {
-      console.log('boom', err);
-      next(err);
+
+
+// GET/celebrities/new
+router.get('/celebrities/new', (req, res, next) => {
+  res.render('celebrities/new', {
+  })
+})
+  
+// POST /celebrities/new
+router.post('/celebrities/new', (req, res, next) => {
+  Celebrity.create({
+    name: req.body.name, 
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase,
+  }).then(newCelebrity => {
+    
+    res.redirect('/celebrities');
+  }).catch(err => {
+    res.redirect('celebrities/new')
+    // next(err);
+  })
+})
+
+router.get('/celebrities/:id', (req, res, next) => {
+  const id = req.params.id // le nom doit être le même que celui de la route
+
+  Celebrity.findOne({_id: id})
+  .then((celebrity) => {
+    console.log('celebrity', celebrity)  
+    res.render('celebrities/show', {
+      celebrity: celebrity
     })
   })
-  
-
-
+  .catch(err => {
+    console.log('boom', err);
+    next(err);
+  })
+})
 
 module.exports = router;
