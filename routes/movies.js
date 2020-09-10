bdfd454c54590662165901d71cfd49f7a8e7bfc1
kardelen-karatas <kardelen.karatas@gmail.com>
2020-09-10 20:06:32 +0200

    iteration 7 8

---
+++ routes/movies.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router  = express.Router();
+
+const Celebrity = require('../models/Celebrity.model');
+const Movie = require('../models/Movie.model');
+
+
+router.get('/movies/new', (req, res, next) =>{
+
+    Celebrity.find({})
+    .then(
+        celebrities => {
+            res.render('movies/new', {
+                celebrities:celebrities})
+    })
+    .catch(err=>next(err))
+})
+
+router.post('/movies', (req, res, next) => {
+    const {title, genre, plot, cast} = req.body // destructuring
+    
+    console.log(cast) // ["1234", "4567"]
+    
+    Movie.create({title, genre, plot, cast})
+    .then(res.redirect('/movies'))
+    .catch(err=>next(err))
+})
+
+router.get('/movies', (req, res, next) =>{
+    res.send('MOVIES PAGE')
+})
+module.exports = router;
