2b080ad48f8f40eb8e6edfb679cb8d02dc2f213c
Sarah Damag <sarahdamag@gmail.com>
2020-09-15 14:37:06 +0200

    finished

--- routes/movies.js
+++ routes/movies.js
@@ -44,6 +44,59 @@ router.post('/movies/new', (req, res, next) => {
       .catch((err) => next(err));
 });
 
+//DELETE A MOVIE
+//iteration 11
+router.post('/movies/:id/delete', (req, res, next) => {
+   const id = req.params.id;
+   Movie.findByIdAndRemove(id)
+      .then(() => res.redirect('/movies'))
+      .catch((err) => next(err));
+});
+
+//EDIT A MOVIE
+//iteration 12
+router.get('/:id/edit', (req, res, next) => {
+   const id = req.params.id;
+   Movie.findOne({ _id: id })
+      .populate('cast')
+      .then((movie) => {
+         //chercher la liste des célébrities
+         Celebrity.find({})
+            .then((celebrities) => {
+               celebrities.forEach((celebrity, i) => {
+                  const movieCastIds = movie.cast.map((celebrity) => celebrity.id); // ["1234", "3456", ...] pour ne récuperer que les id des célébrités
+                  if (movieCastIds.includes(celebrity.id)) {
+                     //movie.cast[i]._id et un objet, celebrity._id est une string
+                     celebrity.selected = true;
+                  }
+               });
+               res.render('movies/edit', {
+                  movie: movie,
+                  celebrity: celebrities,
+               });
+            })
+            .catch((err) => {
+               next(err);
+            });
+      })
+      .catch((err) => {
+         next(err);
+      });
+});
+
+router.post('/:id/edit', (req, res, next) => {
+   const { title, genre, plot, cast } = req.body;
+   Movie.findByIdAndUpdate(
+      req.params.id,
+      { title: title, genre: genre, plot: plot, cast: cast },
+      { new: true }
+   )
+      .then(() => {
+         res.redirect('/movies');
+      })
+      .catch((err) => next(err));
+});
+
 //VOIR LE DETAIL DE CHAQUE MOVIE
 //iteration 10
 router.get('/movies/:id', (req, res, next) => {
@@ -51,7 +104,6 @@ router.get('/movies/:id', (req, res, next) => {
    Movie.findOne({ _id: id })
       .populate('cast')
       .then((movie) => {
-         console.log(movie);
          res.render('movies/show', { movie: movie });
       })
       .catch((err) => next(err));
